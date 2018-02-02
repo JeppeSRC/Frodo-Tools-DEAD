ee05fe1e2c1c42d374153446c781d28acb0bb7c8
JeppeSRC <jeppe97rc@gmail.com>
2018-02-02 21:56:50 +0100

    Adding and removing files

---
+++ FDconverter/File.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+
+namespace FDconverter {
+
+    public enum FileType  {
+        Unknown,
+        Image,
+        Model
+    };
+
+    public class FDFile : INotifyPropertyChanged {
+
+        private struct FileExtension {
+            public FileExtension(string extension, FileType type) { this.extension = extension; this.type = type; }
+            public string extension { get; }
+            public FileType type { get; }
+        };
+
+        private static FileExtension[] extensions = { new FileExtension(".png", FileType.Image),
+                                                      new FileExtension(".jpg", FileType.Image),
+                                                      new FileExtension(".bmp", FileType.Image)};
+
+        private static FileType GetFileTypeFromExtension(String filename) {
+
+            for (uint i = 0; i < extensions.Length; i++) {
+                if (filename.EndsWith(extensions[i].extension)) {
+                    return extensions[i].type;
+                }
+            }
+
+            return FileType.Unknown;
+        }
+
+        private int _progress;
+        private FileType _type;
+
+        public string path { get; }
+        public long size { get; }
+        public string sizeString { get; set; }
+        public FileType type { get { return _type; } set { _type = value; OnPropertyChanged(); } }
+        public int progress { get { return _progress; } set { _progress = value; OnPropertyChanged(); } }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        
+        public FDFile(string path, long size) {
+            this.path = path;
+            this.size = size;
+            this.type = GetFileTypeFromExtension(path);
+            progress = 0;
+
+            if (size >= 1024 * 1024 * 1024) {
+                sizeString = string.Format("{0:F2} GB", (float)size / (1024.0f * 1024.0f * 1024.0f));
+            } else if (size >= 1024 * 1024) {
+                sizeString = string.Format("{0:F2} MB", (float)size / (1024.0f * 1024.0f));
+            } else if (size >= 1024) {
+                sizeString = string.Format("{0:F2} KB", (float)size / (1024.0f));
+            } else {
+                sizeString = string.Format("{0:F2} Bytes", size);
+            }
+        }
+
+       protected virtual void OnPropertyChanged([CallerMemberName] string name = "") {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+       }
+
+    }
+}
--- FDconverter/MainWindow.xaml.cs
+++ FDconverter/MainWindow.xaml.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.IO;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -17,9 +19,107 @@ namespace FDconverter {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+
     public partial class MainWindow : Window {
+
+        void Sort(ref List<int> indices) {
+            List<int> tmp = new List<int>(indices);
+
+            for (int i = 0; i < indices.Count; i++) {
+                int max = 0;
+
+                for (int j = 0; j < tmp.Count; j++) {
+                    if (tmp[j] > max) max = tmp[j];
+                }
+
+                tmp.Remove(max);
+
+                indices[i] = max;
+            }
+        }
+
+        private ObservableCollection<FDFile> files;
+
         public MainWindow() {
             InitializeComponent();
+
+            files = new ObservableCollection<FDFile>();
+
+            lvFiles.ItemsSource = files;
+            
+        }
+
+        private void AddFiles(String[] files) {
+            for (int i = 0; i < files.Length; i++) {
+                try {
+                    FileStream file = new FileStream(files[i], FileMode.Open);
+                    this.files.Add(new FDFile(files[i], file.Length));
+                    file.Close();
+                } catch(Exception e) {
+                    Console.WriteLine(e.ToString());
+                }
+            }
+        }
+
+        private void lvFiles_Drop(object sender, DragEventArgs e) {
+            String[] s = (String[])e.Data.GetData(DataFormats.FileDrop);
+            AddFiles(s);           
+        }
+
+        private void lvFiles_KeyDown(object sender, KeyEventArgs e) { 
+            if (e.Key == Key.Delete) {
+                var tmp = lvFiles.SelectedItems;
+
+                List<int> indices = new List<int>();
+
+                for (int i = 0; i < tmp.Count; i++) {
+                    for (int j = 0; j < files.Count; j++) {
+                        if (tmp[i] == files[j]) indices.Add(j);
+                    }
+                }
+
+                Sort(ref indices);
+
+                for (int i = 0; i < indices.Count; i++) {
+                    files.RemoveAt(indices[i]);
+                }
+            }
+        }
+
+        private void Button_Click_Add(object sender, RoutedEventArgs e) {
+           System.Windows.Forms.OpenFileDialog diag = new System.Windows.Forms.OpenFileDialog();
+
+            diag.CheckFileExists = true;
+            diag.CheckPathExists = true;
+            diag.Multiselect = true;
+            diag.Title = "Add Files";
+
+            if (diag.ShowDialog() == System.Windows.Forms.DialogResult.OK) {
+                AddFiles(diag.FileNames);
+            }
+        }
+
+        private void Button_Click_Remove(object sender, RoutedEventArgs e) {
+            var tmp = lvFiles.SelectedItems;
+
+            List<int> indices = new List<int>();
+
+            for (int i = 0; i < tmp.Count; i++) {
+                for (int j = 0; j < files.Count; j++) {
+                    if (tmp[i] == files[j]) indices.Add(j);
+                }
+            }
+
+            Sort(ref indices);
+
+            for (int i = 0; i < indices.Count; i++) {
+                files.RemoveAt(indices[i]);
+            }
+        }
+
+        private void lvFiles_MouseDown(object sender, MouseButtonEventArgs e) {
+
         }
     }
 }
