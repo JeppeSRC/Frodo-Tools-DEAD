d69376147ac10235e0f141aa5c42b0fb7a685386
JeppeSRC <jeppe97rc@gmail.com>
2018-02-04 01:55:22 +0100

    Renamed FDconverter to FDconverterUI and created new project FDconverter

---
+++ FDconverter/src/assembly.cpp
@@ -0,0 +1,7 @@
+using namespace System;
+using namespace System::Reflection;
+using namespace System::Runtime::CompilerServices;
+using namespace System::Runtime::InteropServices;
+using namespace System::Security::Permissions;
+
+[assembly:AssemblyTitleAttribute(L"Frodo Converter DLL")];
---
+++ FDconverter/src/converter/converter.h
@@ -0,0 +1,12 @@
+#pragma once
+
+
+namespace FDconverter {
+
+public ref class FDConverter {
+
+
+
+};
+
+}
